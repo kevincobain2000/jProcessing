41031b3ebf1164f491df65ebc0ea5b981c713aa4
Pulkit Kathuria <kevincobain2000@gmail.com>
2012-03-07 20:33:54 +0900

    automatic push committed

--- jnlp/forkme.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-fork_me = """<a href="http://github.com/kevincobain2000/jProcessing"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/ce742187c818c67d98af16f96ed21c00160c234a/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677261795f3664366436642e706e67" alt="Fork me on GitHub"></a>"""
-f = open('index.html','a')
-f.write(fork_me)
-f.close()
